0ed407b85375334e32ed64dc24f59e821355eb79
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-08-01 14:12:39 -0600

    trying to link the files to get play function to work onclick, not working thus far

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 function play(playerChoice) {
     let computerChoice = "Rock";
     if (playerChoice == "Rock" && computerChoice == "Rock") {
-        return "tie!";
+        return console.log("tie!");
     }
     if (playerChoice == "Paper" && computerChoice == "Rock") {
-        return "Win!";
+        return console.log("Win!");
     }
     if (playerChoice == "Scissors" && computerChoice == "Rock") {
-        return "You lost this one";
+        return console.log("You lost this one");
     }
     if (playerChoice == "Scissors" && computerChoice == "Scissors") {
-        return "Tie!";
+        return console.log("Tie!");
     }
     if (playerChoice == "Rock" && computerChoice == "Scissors") {
-        return "Win";
+        return console.log("Win");
     }
     if (playerChoice == "Paper" && computerChoice == "Scissors") {
-        return "You lost this one";
+        return console.log("You lost this one");
     }
     if (playerChoice == "Rock" && computerChoice == "Paper") {
-        return "You lost this one";
+        return console.log("You lost this one");
     }
 
     if (playerChoice == "Paper" && computerChoice == "Paper") {
-        return "Tie";
+        return console.log("Tie");
     }
     if (playerChoice == "Scissors" && computerChoice == "Paper") {
-        return "Win!";
+        return console.log("Win!");
     }
+}
